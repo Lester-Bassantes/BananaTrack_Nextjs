e39cd677aeedd7d3913328e86a524caf0735c59d
Lester <67484367+Lester-Bassantes@users.noreply.github.com>
2024-12-06 15:15:25 -0500

    Add user controller

---
+++ app/controllers/userController.js
@@ -0,0 +1,20 @@
+import User from '../models/User';
+import bcrypt from 'bcrypt';
+
+export async function createUser(user) {
+    try {
+        const { user_name, user_second_name, user_username, user_birthday, user_password } = user;
+        const hashedPassword = await bcrypt.hash(user_password, 10);
+        const newUser = await User.create({
+            user_name,
+            user_second_name,
+            user_username,
+            user_birthday,
+            user_password: hashedPassword,
+        });
+        return res.status(200).json({ message: 'Usuario creado exitosamente' });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Error al crear el usuario' });
+    }
+}
--- app/register/storeNewUser.js
+++ app/register/storeNewUser.js
@@ -1,12 +1,13 @@
 'use server';
 
+import { createUser } from '../controllers/userController';
 export async function createNewUser(formData) {
     const user = {
-        name: formData.get('txtFirstName'),
-        secondName: formData.get('txtSecondName'),
-        username: formData.get('txtUsername'),
-        birthday: formData.get('dateBirthday'),
-        password: formData.get('txtPassword'),
+        user_name: formData.get('txtFirstName'),
+        user_second_name: formData.get('txtSecondName'),
+        user_username: formData.get('txtUsername'),
+        user_birthday: formData.get('dateBirthday'),
+        user_password: formData.get('txtPassword'),
     }
 
     console.log(user);
